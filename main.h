6c8928ef4884053d4d27774f22e5454d9e91f93d
theadnanerfan <theadnanerfan@gmail.com>
2023-03-26 12:12:46 -0700

    Update

--- main.h
+++ main.h
@@ -1,9 +1,23 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
-#include <unistd.h>
+
 #include <stdarg.h>
 
+int _putchar(char c);
 int _printf(const char *format, ...);
+int print_char(va_list args);
+int print_string(va_list args);
+int print_percent(va_list args);
 
-#endif
+/**
+ * struct format_t - struct for format and function
+ * @type: The format
+ * @f: The function
+ */
+typedef struct format_t
+{
+    char type;
+    int (*f)(va_list);
+} format_t;
+
+#endif /* MAIN_H */
