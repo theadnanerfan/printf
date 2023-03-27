470c71aee174905fb3823016a7a4fa73bc5ca317
theadnanerfan <theadnanerfan@gmail.com>
2023-03-27 03:28:13 -0700

    Update

--- _printf.c
+++ _printf.c
@@ -1,66 +1,55 @@
 #include "main.h"
-
-void print_buffer(char buffer[], int *indexBuff);
+#include <stdio.h>
 
 /**
- * _printf - printf custom
- * @format: format pointer
- * Return: characters written
+ * _printf - produces output according to a format
+ * @format: character string containing zero or more directives
+ *
+ * Return: the number of characters printed (excluding the null byte used to end output to strings)
  */
 int _printf(const char *format, ...)
 {
-	int i;
-	int prnt = 0, prtChars = 0, indexBuff = 0;
-	int flags, width, precision, size;
-	va_list variadlist;
-	char buffer[BufferSize];
+    va_list args;
+    int printed_chars = 0;
+    const char *p;
 
-	if (format == NULL)
-		return (-1);
+    va_start(args, format);
 
-	va_start(variadlist, format);
+    for (p = format; *p != '\0'; p++)
+    {
+        if (*p != '%')
+        {
+            putchar(*p);
+            printed_chars++;
+        }
+        else
+        {
+            p++;
+            if (*p == '%')
+            {
+                putchar('%');
+                printed_chars++;
+            }
+            else if (*p == 'c')
+            {
+                char c = va_arg(args, int);
+                putchar(c);
+                printed_chars++;
+            }
+            else if (*p == 's')
+            {
+                char *s = va_arg(args, char *);
+                int i;
+                for (i = 0; s[i] != '\0'; i++)
+                {
+                    putchar(s[i]);
+                    printed_chars++;
+                }
+            }
+        }
+    }
 
-	for (i = 0; format && format[i] != '\0'; i++)
-	{
-		if (format[i] != '%')
-		{
-			buffer[indexBuff++] = format[i];
-			if (indexBuff == BufferSize)
-				print_buffer(buffer, &indexBuff);
-			prtChars++;
-		}
-		else
-		{
-			print_buffer(buffer, &indexBuff);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, variadlist);
-			precision = get_precision(format, &i, variadlist);
-			size = get_size(format, &i);
-			++i;
-			prnt = handle_print(format, &i, variadlist, buffer,
-				flags, width, precision, size);
-			if (prnt == -1)
-				return (-1);
-			prtChars += prnt;
-		}
-	}
+    va_end(args);
 
-	print_buffer(buffer, &indexBuff);
-	
-	va_end(variadlist);
-
-	return (prtChars);
-}
-
-/**
- * print_buffer - prints buffer contents
- * @buffer: char[]
- * @indexBuff: buffers index
- */
-void print_buffer(char buffer[], int *indexBuff)
-{
-	if (*indexBuff > 0)
-		write(1, &buffer[0], *indexBuff);
-
-	*indexBuff = 0;
+    return printed_chars;
 }
