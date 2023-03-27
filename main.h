7eb8a3d5d0840eb0a27a5aed7f919e6ca9b43e09
theadnanerfan <theadnanerfan@gmail.com>
2023-03-27 16:22:25 -0700

    Update

--- main.h
+++ main.h
@@ -45,7 +45,6 @@ int printstring(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 int printpercent(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
-
 int printint(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 int printbin(va_list types, char buffer[],
@@ -58,15 +57,12 @@ int print_unsignedhex(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 int print_upperhex(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
-
 int printhex(va_list types, char map_to[],
 char buffer[], int flags, char flag_ch, int width, int precision, int size);
-
 int getflag(const char *format, int *i);
 int getwidth(const char *format, int *i, va_list list);
 int getprec(const char *format, int *i, va_list list);
 int getsize(const char *format, int *i);
-
 int writechar(char c, char buffer[],
 	int flags, int width, int precision, int size);
 int writestring(int is_positive, int ind, char buffer[],
@@ -75,15 +71,12 @@ int writenum(int ind, char bff[], int flags, int width, int precision,
 	int length, char padd, char extra_c);
 int writeptr(char buffer[], int ind, int length,
 	int width, int flags, char padd, char extra_c, int padd_start);
-
 int write_unsigned(int is_negative, int ind,
 char buffer[],
 	int flags, int width, int precision, int size);
-
 int printable(char);
 int asccihex(char, char[], int);
 int isnum(char);
-
 long int numsize(long int num, int size);
 long int unsignedsize(unsigned long int num, int size);
 
